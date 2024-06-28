6770a4f971096d4e46f21f281de879060232c9c3
Namfon <varisa.vb@hotmail.com>
2024-06-28 16:48:30 +0700

    feat: Add 4 API ex.1-5 without validation

--- server/apps/products.js
+++ server/apps/products.js
@@ -1,15 +1,42 @@
 import { Router } from "express";
+import { db } from "../utils/db.js";
+import { ObjectId } from "mongodb";
+
+const collection = db.collection("products");
 
 const productRouter = Router();
 
-productRouter.get("/", (req, res) => {});
+productRouter.get("/", async (req, res) => {
+  const result = await collection.find({}).toArray();
+  return res.status(200).json({ data: result });
+});
 
-productRouter.get("/:id", (req, res) => {});
+productRouter.get("/:id", async (req, res) => {});
 
-productRouter.post("/", (req, res) => {});
+productRouter.post("/", async (req, res) => {
+  const result = await collection.insertOne(req.body);
+  return res.status(201).json({
+    message: "Product has been created successfully",
+  });
+});
 
-productRouter.put("/:id", (req, res) => {});
+productRouter.put("/:id", async (req, res) => {
+  const productId = new ObjectId(req.params.id);
+  const result = await collection.updateOne(
+    { _id: productId },
+    { $set: req.body }
+  );
+  return res.status(200).json({
+    message: "Product has been updated successfully",
+  });
+});
 
-productRouter.delete("/:id", (req, res) => {});
+productRouter.delete("/:id", async (req, res) => {
+  const productId = new ObjectId(req.params.id);
+  const result = await collection.deleteOne({ _id: productId });
+  return res.status(200).json({
+    message: "Product has been deleted successfully",
+  });
+});
 
 export default productRouter;
--- server/utils/db.js
+++ server/utils/db.js
@@ -1,1 +1,12 @@
 // Set up db connection here
+import { MongoClient } from "mongodb";
+
+const connectionString = "mongodb://127.0.0.1:27017";
+
+const client = new MongoClient(connectionString, {
+  useUnifiedTopology: true,
+});
+
+await client.connect();
+
+export const db = client.db("practice-mongo");
