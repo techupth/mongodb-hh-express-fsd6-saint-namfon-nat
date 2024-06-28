a0fdbc5b5c0c5d7425c5d691f0387bc6e7b3ee1b
Kittanut Thammapibaljit <tkittanut@gmail.com>
2024-06-28 14:57:11 +0700

    fix bug

--- server/apps/products.js
+++ server/apps/products.js
@@ -5,14 +5,40 @@ import { ObjectId } from "mongodb";
 const productRouter = Router();
 
 productRouter.get("/", async (req, res) => {
+  const name = req.query.name;
+  const description = req.query.description;
+  const category = req.query.category;
+
+  const query = {
+    $or: [],
+  };
+
+  if (name) {
+    query.$or.push({ name: new RegExp(name, "i") });
+  }
+  if (description) {
+    query.$or.push({ description: new RegExp(description, "i") });
+  }
+  if (category) {
+    query.$or.push({ category: new RegExp(category, "i") });
+  }
+
   const collection = db.collection("products");
 
-  const products = await collection.find().toArray();
+  const products = await collection.find(query).limit(10).toArray();
 
   return res.json({ data: products });
 });
 
-productRouter.get("/:id", (req, res) => {});
+productRouter.get("/:id", async (req, res) => {
+  const collection = db.collection("products");
+
+  const productId = new ObjectId(req.params.id);
+
+  const product = await collection.find({ _id: productId }).toArray();
+
+  return res.json({ data: product[0] });
+});
 
 productRouter.post("/", async (req, res) => {
   const collection = db.collection("products");
